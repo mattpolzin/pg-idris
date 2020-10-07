f6693e0f22caaef7e75f6634602a70c073668d52
Mathew Polzin <matt.polzin@gmail.com>
2020-10-06 22:43:24 -0700

    complete transition to makefiles and drop workaround that apparently is not actually needed afterall.

--- Test/build.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-export LDFLAGS="-L/usr/local/Cellar/idris2/0.2.1/libexec/idris2-0.2.1/pg-idris/lib"
-export CPPFLAGS="-I/usr/local/Cellar/idris2/0.2.1/libexec/idris2-0.2.1/pg-idris/lib"
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/Cellar/idris2/0.2.1/libexec/idris2-0.2.1/pg-idris/lib
-
-idris2 Test.idr -p pg-idris -p contrib -o pg-test
